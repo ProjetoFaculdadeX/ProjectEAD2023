749dd1fadbc848f75cf23cd4511fc5b521290d26
Juliofreitas93 <juliocesarfr.005@hotmail.com>
2023-09-06 11:03:46 -0300

    Subida DB

---
+++ ProjectDBO2023/ProjectDBO2023/Security/Certificado.sql
@@ -0,0 +1,5 @@
+﻿CREATE CERTIFICATE [Certificado]
+    AUTHORIZATION [dbo]
+    ENCRYPTION BY PASSWORD = N'dbkeiNkvlyOp|lPqO2ntoaqcmsFT7_&#$!~<M+dw7g{|uKoj'
+    WITH SUBJECT = N'Certificado Senha Usuario', START_DATE = N'2023-09-06T13:26:00', EXPIRY_DATE = N'2024-09-06T13:26:00';
+
