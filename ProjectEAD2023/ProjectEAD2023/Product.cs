bb48863b1a17791bad5aee54bb1822acc47a8321
Juliofreitas93 <juliocesarfr.005@hotmail.com>
2023-10-11 22:29:59 -0300

    nova classe

---
+++ ProjectEAD2023/ProjectEAD2023/Product.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectEAD2023
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string? Description { get; set; }
+        public string? Lote { get; set; }
+        public DateTime Validade { get; set; }
+        public int Quantity { get; set; }
+
+    }
+  
+}
